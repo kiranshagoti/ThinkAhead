86b15b1e4300844ed75fcac9d773a25f30b4968b
Saga Åkermark <sagaakermark@hotmail.com>
2019-10-03 19:39:09 +0200

    really nothing to comit

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
-import React from 'react';
-import './App.css';
-import Articles from './components/Articles';
+import React from "react";
+import "./App.css";
+import Articles from "./components/Articles";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Team from "./components/Team";
 import { Route, Switch } from "react-router-dom";
-
-
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import SideBar from "./components/SideBar";
 
 const NotFound = () => {
   return <div>Page not found</div>;
 };
-
 
 function App() {
   return (
@@ -25,60 +24,63 @@ function App() {
         <Route path="/team" component={Team} />
         <Articles />
         <Route component={NotFound} />
-        </Switch>
+      </Switch>
     </div>
   );
-=======
-import React from "react";
-// import Navbar from "./components/Navbar";
-import { Route, Switch } from "react-router-dom";
-// import Projects from "./components/Projects";
-// import ProjectDetails from "./components/ProjectDetails";
-// import TaskDetails from "./components/TaskDetails";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import SideBar from "./components/SideBar";
+  // import React from "react";
+  // // import Navbar from "./components/Navbar";
+  // import { Route, Switch } from "react-router-dom";
+  // // import Projects from "./components/Projects";
+  // // import ProjectDetails from "./components/ProjectDetails";
+  // // import TaskDetails from "./components/TaskDetails";
+  // import Signup from "./components/Signup";
+  // import Login from "./components/Login";
+  // import SideBar from "./components/SideBar";
 
-// import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
+  // // import "bootstrap/dist/css/bootstrap.css";
+  // import "./App.css";
 
-class App extends React.Component {
-  state = {
-    user: this.props.user
-  };
+  class App extends React.Component {
+    state = {
+      user: this.props.user
+    };
 
-  setUser = user => {
-    this.setState({
-      user: user
-    });
-  };
+    setUser = user => {
+      this.setState({
+        user: user
+      });
+    };
 
-  render() {
-    return (
-
-      <div className="App">
-      <h1>HOME</h1>
-        {/* <Navbar user={this.state.user} setUser={this.setUser} />
+    render() {
+      return (
+        <div className="App">
+          <h1>HOME</h1>
+          {/* <Navbar user={this.state.user} setUser={this.setUser} />
         <Route exact path="/projects" component={Projects} /> */}
 
-        {/* <Route
+          {/* <Route
           exact
           path="/projects/:id"
           render={props => <ProjectDetails {...props} user={this.state.user} />}
         /> */}
 
-        {/*  */}
-        <Switch>
-        <Route exact path="/signup" render={props => <Signup setUser={this.setUser} {...props} />}
-        />
-        <Route exact path="/login" render={props => <Login setUser={this.setUser} {...props} />}
-        />
-        </Switch>
-        <Route exact path="/funeral" component={SideBar} />
-              </div>
-    );
+          {/*  */}
+          <Switch>
+            <Route
+              exact
+              path="/signup"
+              render={props => <Signup setUser={this.setUser} {...props} />}
+            />
+            <Route
+              exact
+              path="/login"
+              render={props => <Login setUser={this.setUser} {...props} />}
+            />
+          </Switch>
+          <Route exact path="/funeral" component={SideBar} />
+        </div>
+      );
+    }
   }
->>>>>>> 667e8a4db02965e61de9caf75e655feb7d8a61df
 }
-
 export default App;
