862af4bf7b67c553ba3e8c0cc503db2840741a09
KaterynaStadnyk <kateryna.stad@gmail.com>
2019-10-03 11:22:00 +0200

    Loginalldone

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router-dom";
+// import Projects from "./components/Projects";
+// import ProjectDetails from "./components/ProjectDetails";
+// import TaskDetails from "./components/TaskDetails";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    user: this.props.user
+  };
+
+  setUser = user => {
+    this.setState({
+      user: user
+    });
+  };
+
+  render() {
+    return (
+
+      <div className="App">
+      <h1>HOME</h1>
+        {/* <Navbar user={this.state.user} setUser={this.setUser} />
+        <Route exact path="/projects" component={Projects} /> */}
+
+        {/* <Route
+          exact
+          path="/projects/:id"
+          render={props => <ProjectDetails {...props} user={this.state.user} />}
+        /> */}
+
+        {/*  */}
+        <Switch>
+        <Route
+          exact
+          path="/signup"
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact
+          path="/login"
+          render={props => <Login setUser={this.setUser} {...props} />}
+        />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
